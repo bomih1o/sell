0d2b3c5144414324871ca5611dc3dfa029febe12
bomih1o <voqn19bdn67454@126.com>
2021-03-20 06:38:16 +0800

    ProductInfo

---
+++ src/main/java/com/team/sell/repository/ProductInfoRepository.java
@@ -0,0 +1,11 @@
+package com.team.sell.repository;
+
+import com.team.sell.pojo.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String> {
+
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+}
