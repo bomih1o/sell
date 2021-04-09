6b996350cfc75f80d0e98d1d35f79de4ac54510a
bomih1o <voqn19bdn67454@126.com>
2021-04-09 20:33:56 +0800

    OrderMaster

--- src/main/java/com/team/sell/service/ProductInfoService.java
+++ src/main/java/com/team/sell/service/ProductInfoService.java
@@ -1,5 +1,6 @@
 package com.team.sell.service;
 
+import com.team.sell.dto.CartDTO;
 import com.team.sell.pojo.ProductInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,6 +25,9 @@ public interface ProductInfoService {
     ProductInfo save(ProductInfo productInfo);
 
     // 加库存
+    void increaseStock(List<CartDTO> cartDTOList);
+
     // 减库存
+    void decreaseStock(List<CartDTO> cartDTOList);
 
 }
--- src/main/java/com/team/sell/service/impl/OrderServiceImpl.java
+++ src/main/java/com/team/sell/service/impl/OrderServiceImpl.java
@@ -1,10 +1,22 @@
 package com.team.sell.service.impl;
 
+import com.team.sell.dto.CartDTO;
 import com.team.sell.dto.OrderDTO;
+import com.team.sell.enums.OrderStatusEnum;
+import com.team.sell.enums.PayStatusEnum;
+import com.team.sell.enums.ResultEnum;
+import com.team.sell.exception.SellException;
 import com.team.sell.pojo.OrderDetail;
+import com.team.sell.pojo.OrderMaster;
+import com.team.sell.pojo.ProductInfo;
+import com.team.sell.repository.OrderDetailRepository;
+import com.team.sell.repository.OrderMasterRepository;
 import com.team.sell.service.OrderService;
+import com.team.sell.service.ProductInfoService;
 import com.team.sell.utils.KeyUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,11 +25,20 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
 
     @Override
     @Transactional
@@ -27,10 +48,40 @@ public class OrderServiceImpl implements OrderService {
         BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
         List<OrderDetail> list = orderDTO.getOrderDetailList();
         // 1.查询商品（数量，价格）
-        for (OrderDetail od: list ) {
+        for (OrderDetail orderDetail: list ) {
+            ProductInfo productInfo = productInfoService.findOne(orderDetail.getProductId());
+            if(productInfo == null){
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
 
+            // 2. 计算订单总价
+            orderAmount = productInfo.getProductPrice()
+                    .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                    .add(orderAmount);
+
+            // 订单详情入库
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo, orderDetail);
+            orderDetailRepository.save(orderDetail);
         }
-        return null;
+
+        //3. 写入订单数据库
+        orderDTO.setOrderId(orderId);
+        OrderMaster orderMaster = new OrderMaster();
+        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderAmount(orderAmount);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterRepository.save(orderMaster);
+
+        //4. 扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e ->
+            new CartDTO(e.getProductId(),e.getProductQuantity())
+        ).collect(Collectors.toList());
+        productInfoService.decreaseStock(cartDTOList);
+
+        return orderDTO;
     }
 
     @Override
