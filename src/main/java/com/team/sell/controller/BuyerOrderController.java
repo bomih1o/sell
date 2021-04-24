55136e3ab45387eb884115c330c5863997c3640c
bomih1o <voqn19bdn67454@126.com>
2021-04-24 17:57:54 +0800

    order controller

---
+++ src/main/java/com/team/sell/controller/BuyerOrderController.java
@@ -0,0 +1,52 @@
+package com.team.sell.controller;
+
+import com.team.sell.converter.OrderForm2OrderDTOConverter;
+import com.team.sell.dto.OrderDTO;
+import com.team.sell.enums.ResultEnum;
+import com.team.sell.exception.SellException;
+import com.team.sell.form.OrderForm;
+import com.team.sell.service.OrderService;
+import com.team.sell.utils.ResultVoUtil;
+import com.team.sell.vo.ResultVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/buyer/order")
+@Slf4j
+public class BuyerOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping("/create")
+    public ResultVo<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            log.error("【创建订单】参数不正确，orderForm={}", orderForm);
+            throw new SellException(ResultEnum.PARAM_ERROR.getCode(),
+                    bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if(CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+            log.error("【创建订单】购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+
+        OrderDTO createResult = orderService.create(orderDTO);
+
+        Map<String,String> map = new HashMap<>();
+        map.put("orderId",createResult.getOrderId());
+
+        return ResultVoUtil.success(map);
+    }
+
+}
