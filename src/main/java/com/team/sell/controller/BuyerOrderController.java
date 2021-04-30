53a8b84dfeca342c8e7b7dab9d1962adf3864c65
bomih1o <voqn19bdn67454@126.com>
2021-04-30 17:16:14 +0800

    order controller

--- src/main/java/com/team/sell/controller/BuyerOrderController.java
+++ src/main/java/com/team/sell/controller/BuyerOrderController.java
@@ -5,18 +5,22 @@ import com.team.sell.dto.OrderDTO;
 import com.team.sell.enums.ResultEnum;
 import com.team.sell.exception.SellException;
 import com.team.sell.form.OrderForm;
+import com.team.sell.service.BuyerService;
 import com.team.sell.service.OrderService;
 import com.team.sell.utils.ResultVoUtil;
 import com.team.sell.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,7 +31,10 @@ public class BuyerOrderController {
     @Autowired
     private OrderService orderService;
 
-    @RequestMapping("/create")
+    @Autowired
+    private BuyerService buyerService;
+
+    @PostMapping("/create")
     public ResultVo<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult){
         if (bindingResult.hasErrors()){
             log.error("【创建订单】参数不正确，orderForm={}", orderForm);
@@ -44,9 +51,41 @@ public class BuyerOrderController {
         OrderDTO createResult = orderService.create(orderDTO);
 
         Map<String,String> map = new HashMap<>();
-        map.put("orderId", createResult.getOrderId())
+        map.put("orderId", createResult.getOrderId());
 
         return ResultVoUtil.success(map);
     }
+
+    //订单列表
+    @GetMapping("/list")
+    public ResultVo<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size ){
+        if(StringUtils.isEmpty(openid)){
+            log.error("【查询订单列表】openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, pageRequest);
+
+        return ResultVoUtil.success(orderDTOPage.getContent());
+    }
+
+    //订单详情
+    @GetMapping("/detail")
+    public ResultVo<OrderDTO> detail(@RequestParam("openid") String openid,
+                                     @RequestParam("orderId") String orderId){
+        OrderDTO orderDTO = buyerService.findOrderOne(openid,orderId);
+        return ResultVoUtil.success(orderDTO);
+    }
+
+    // 取消订单
+    @PostMapping("/cancel")
+    public ResultVo cancel(@RequestParam("openid") String openid,
+                            @RequestParam("orderId") String orderId){
+        buyerService.cancelOrder(openid,orderId);
+        return ResultVoUtil.success();
+    }
+
 
 }
