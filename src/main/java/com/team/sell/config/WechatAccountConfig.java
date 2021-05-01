ff94117e4c65b18065ef7312426d980c4c291d2e
bomih1o <voqn19bdn67454@126.com>
2021-05-01 22:30:29 +0800

    OrderMaster

---
+++ src/main/java/com/team/sell/config/WechatAccountConfig.java
@@ -0,0 +1,22 @@
+package com.team.sell.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "wechat")
+public class WechatAccountConfig {
+
+    /**
+     * 公众平台id
+     */
+    private String mpAppId;
+    /**
+     * 公众平台密钥
+     */
+    private String mpAppSecret;
+
+}
