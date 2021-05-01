58efaa86db548b7dabb411ddd47ff3560eb28610
bomih1o <voqn19bdn67454@126.com>
2021-05-01 17:50:03 +0800

    order controller

--- src/main/java/com/team/sell/controller/WeixinController.java
+++ src/main/java/com/team/sell/controller/WeixinController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/weixin")
@@ -15,6 +16,13 @@ public class WeixinController {
     public void auth(@RequestParam("code") String code){
         log.info("进入auth方法");
         log.info("code = {}",code);
+        String appID = "wxbbe333bf50b5b8c9";
+        String appsecret = "497193b11eaeb2377fb77e0a172e03fe";
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+
+                appID+"&secret="+appsecret+"&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url,String.class);
+        log.info("response={}",response);
 
     }
 }
